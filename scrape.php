c3c997ee31de1d3d995f559ccd916ccfce7c7e24
Matt Briney <mkbriney@gmail.com>
2016-05-05 12:07:34 -0400

    v0.8
    - Modified MailChimp class to not abort the loop
    - Added more filter cases for email addresses that MailChimp will bounce

--- scrape.php
+++ scrape.php
@@ -19,7 +19,7 @@
         echo "Visitors to ".$venue["name"]."... ";
 
         $visitors = $api->getEndpoint("/venue/".$venue["id"]."/visitors?from=".date("Ymd", time() - 60 * 60 * 24 * 30)."&to=".date("Ymd"));
-        
+
         if ($visitors["response_code"] == "200"){
             echo "\n";
             foreach ($visitors['data']['visitors'] as $visitor){
@@ -28,26 +28,20 @@
                     $Mailchimp = new Mailchimp( $api_key );
                     $Mailchimp_Lists = new Mailchimp_Lists( $Mailchimp );
                     $merge_vars = array("FNAME"=>$visitor["first_name"],"LNAME"=>$visitor["last_name"],'groupings' => array( array("name"=>$master_group,"groups"=> array($sub_group))));
-                    if (strpos($visitor["email"], 'none') === false && strpos($visitor["email"], 'gnail.com'  && strpos($visitor["email"], 'test') === false ){
-                        try {
-                            $subscriber = $Mailchimp_Lists->subscribe( $list_id, array('email' => $visitor["email"]), $merge_vars,'html',$double_optin,TRUE,FALSE);  
+                    if (strpos($visitor["email"], 'none') === false && strpos($visitor["email"], 'gnail.com') === false && strpos($visitor["email"], 'gmail.net') === false && strpos($visitor["email"], 'test') === false ){
+                        //try {
+                            $subscriber = $Mailchimp_Lists->subscribe( $list_id, array('email' => $visitor["email"]), $merge_vars,'html',$double_optin,TRUE,FALSE);
                             echo $subscriber['leid']."\n";
-                        } catch( Mailchimp_ValidationError $e ){
-                            echo "bad email, skipping...\n";
-                        }
-                    } else {
-                         echo "bad email, skipping...\n";
+                        //} catch( Mailchimp_ValidationError $e ){
+                            //echo "bad email, skipping...\n";
+                        //}
+                    //} else {
+                    //     echo "bad email, skipping...\n";
                     }
-                    
+
             }
             echo "\n";
         } else {
             echo "No Visitors\n";
         }
     }
-
-
-
-
-
-  
